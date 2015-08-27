6a1670d57002cfad52f94455a9a2e91101e5bcdb
trikitrok <trikitrok@gmail.com>
2015-08-27 21:21:15 +0200

    Removed unused method

--- card.rb
+++ card.rb
@@ -8,10 +8,6 @@ class Card
     CardDescription.with(face, suit).description
   end
 
-  def description_string
-    face.description + card.suit
-  end
-
   def <=>(other)
     return 0 if self.face.value == other.face.value
     return -1 if self.face.value > other.face.value
