10f14afae8788f09f526461722cf25722f56a8f3
trikitrok <trikitrok@gmail.com>
2015-08-27 18:58:02 +0200

    Moved card creation to inner private class: Deck

--- game.rb
+++ game.rb
@@ -12,9 +12,7 @@ class Card
   FACE_VALUES = ["2","3","4","5","6","7","8","9", "J","Q", "K", "A"]
 
   def self.create(description)
-    face_value = face_value(description)
-    suit = suit_descriptor(description)
-    Card.new(face_value, suit)
+    Deck.extract_card(description)
   end
 
   def description
@@ -28,16 +26,25 @@ class Card
   end
 
   private
-  def self.face_value(description)
-    value_descriptor = value_descriptor(description)
-    FACE_VALUES.find_index(value_descriptor)
-  end
-  def self.value_descriptor(description)
-    String.new(description[0])
-  end
+  class Deck
+    def self.extract_card(description)
+      face_value = face_value(description)
+      suit = suit_descriptor(description)
+      Card.new(face_value, suit)
+    end
 
-  def self.suit_descriptor(description)
-    String.new(description[1])
+    private
+    def self.face_value(description)
+      value_descriptor = value_descriptor(description)
+      FACE_VALUES.find_index(value_descriptor)
+    end
+    def self.value_descriptor(description)
+      String.new(description[0])
+    end
+
+    def self.suit_descriptor(description)
+      String.new(description[1])
+    end
   end
 end
 
