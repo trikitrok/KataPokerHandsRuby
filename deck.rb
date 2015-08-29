f80dbc1a1fa8b56ddd656d86d2dc5a8287a1717f
trikitrok <trikitrok@gmail.com>
2015-08-29 08:51:52 +0200

    Merged describe and create

--- deck.rb
+++ deck.rb
@@ -4,27 +4,25 @@ require "./card_description"
 class Deck
   def self.draw_cards(hand_description)
     card_tokens = parse(hand_description)
-    card_descriptions = describe(card_tokens)
-    create_cards(card_descriptions)
+    create_cards(card_tokens)
   end
 
   private
-  def self.create_cards(card_descriptions)
-    card_descriptions.map do |description|
-      Card.create(description)
+  def self.create_cards(cards_tokens)
+    cards_tokens.map do |token|
+      create_card(token)
     end
+  end
+
+  def self.create_card(card_token)
+    description = CardDescription.new(card_token)
+    Card.create(description)
   end
 
   def self.parse(hand_description)
     card_tokens = hand_description.split(" ")
     validate(card_tokens)
     card_tokens
-  end
-
-  def self.describe(cards_tokens)
-    cards_tokens.map do |token|
-      CardDescription.new(token)
-    end
   end
 
   def self.validate(card_tokens)
