9b65d8b36a24250313eceac8e8874b1efc39c36e
trikitrok <trikitrok@gmail.com>
2015-08-29 09:58:51 +0200

    Hand spec absorbed Deck spec and also created a custom matcher called score

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,13 +1,31 @@
-require './deck'
+require "./exceptions"
 require './hand'
 
 describe "Hand" do
   it "scores the high card" do
-    expect(hand_described_by("3D 2H 5S 9C 2D").score()).to eq("High card: 9C")
-    expect(hand_described_by("2H 3D 5S 9C KD").score()).to eq("High card: KD")
+    expect(hand_described_by("3D 2H 5S 9C 2D")).to score("High card: 9C")
+    expect(hand_described_by("2H 3D 5S 9C KD")).to score("High card: KD")
+  end
+
+  it "never contains a repeated card" do
+    repeated_cards = ["KC", "2H"]
+    expect do
+      hand_described_by("9C KC 2H 5D 2H KC")
+    end.to raise_error(
+      RepeatedCards, "Hand contains these repeated cards: " + repeated_cards.to_s
+    )
   end
 
   def hand_described_by(hand_description)
     Hand.new(hand_description)
+  end
+end
+
+RSpec::Matchers.define :score do |expected|
+  match do |actual|
+    actual.score == expected
+  end
+  failure_message do |actual|
+    "expected '#{actual.score}' score but it actually was '#{expected}'"
   end
 end
