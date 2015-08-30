e1e09dcee1db59fbeb53f5830e14ca8cd828e9f4
trikitrok <trikitrok@gmail.com>
2015-08-30 05:21:27 +0200

    Simplified face

--- card_description.rb
+++ card_description.rb
@@ -6,19 +6,26 @@ class CardDescription
   fields :description
 
   def suit()
-    description_part_for_suit()
+    suit_description
   end
 
   def face()
-    Face.create(description_part_for_face)
+    face_value = compute_face_value()
+    Face.new(face_description, face_value)
   end
 
   private
-  def description_part_for_face()
+  FACE_VALUES = ["2","3","4","5","6","7","8","9","J","Q","K","A"]
+
+  def face_description()
     String.new(description[0])
   end
 
-  def description_part_for_suit()
+  def suit_description()
     String.new(description[1])
+  end
+
+  def compute_face_value()
+    FACE_VALUES.find_index(face_description)
   end
 end
--- face.rb
+++ face.rb
@@ -3,16 +3,4 @@ require "./lib/value_object"
 class Face
   extend ValueObjects::ValueObject
   fields :description, :value
-
-  def self.create(description)
-    value = compute_value(description)
-    Face.new(description, value)
-  end
-
-  private
-  FACE_DESCRIPTIONS = ["2","3","4","5","6","7","8","9","J","Q","K","A"]
-
-  def self.compute_value(description)
-    FACE_DESCRIPTIONS.find_index(description)
-  end
 end
